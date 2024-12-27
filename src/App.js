d0cbb7d1e1ef7e53f0d81b08ffeb306487107d38
Andrea Domeniconi <andrea.domeniconi@outlook.com>
2024-12-27 18:28:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Calendar from './components/Calendar'
+import DayForm from './components/DayForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">EMT</header>
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
