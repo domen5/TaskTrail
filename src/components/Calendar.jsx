98eb5b178be6027ae76ab1a0eb33840f4c6a0f16
Andrea Domeniconi <andrea.domeniconi@outlook.com>
2025-01-04 21:49:09 +0100

    Update Calendar component to track and display the current date and month

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Day from "./Day";
 
 function Calendar() {
-    const days = Array.from({ length: 31 }, (_, index) => index);
+
+    const [selectedDay, setSelectedDay] = useState(new Date());
+    // day n. 0 of next month is the last day of the current month in JS
+    const daysInMonth = new Date(selectedDay.getFullYear(), selectedDay.getMonth() + 1, 0).getDate();
+    const days = Array.from({ length: daysInMonth }, (_, index) => index + 1);
 
     return (
-        <div
-            style={{
-                display: "flex",
-                flexWrap: "wrap",
-            }}
-        >
-            {days.map((day) => (
-                <Day key={day} dayNumber={day} />
-            ))}
+        <div>
+            <div>
+                <p>{selectedDay.toLocaleDateString()}</p>
+                <p>Days in {selectedDay.toLocaleDateString("default", { month: "long" })}: {daysInMonth}</p>
+                <button onClick={() => setSelectedDay(new Date(selectedDay.getFullYear(), selectedDay.getMonth() - 1, 1))}>Previous Month</button>
+                <button onClick={() => setSelectedDay(new Date(selectedDay.getFullYear(), selectedDay.getMonth() + 1, 1))}>Next Month</button>
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                }}
+            >
+                {days.map((day) => (
+                    <Day key={day} dayNumber={day} />		
+                ))}
+            </div>
         </div>
     );
 }
