3fbf10caef70bcdefbc736ab1fba1ef1a5202e4f
Andrea Domeniconi <andrea.domeniconi@outlook.com>
2025-01-16 12:56:43 +0100

    Refactor frontend: centralize backend calls in /src/api/api.js and utility functions in /src/utils/utils.js

---
+++ frontend/src/components/api/api.js
@@ -0,0 +1,37 @@
+import { createKey } from "../../utils/utils";
+
+// Get month data from the backend; Assumes 0 based months
+const fetchMonthData = async (year, month) => {
+    const newMonth = month + 1; // Backend api expects  1-based months
+    const url = `http://localhost:3000/api/worked-hours/${year}/${newMonth}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Failed to fetch month data from backend');
+    }
+    const data = await response.json();
+    return data;
+};
+
+const postDayData = async (date, formData) => {
+    const url = `http://localhost:3000/api/worked-hours`;
+    const dateId = createKey(date);
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            date: dateId,
+            formData: formData
+        })
+    });
+    if (!response.ok) {
+        throw new Error('Failed post day data to backend');
+    }
+    const data = await response.json();
+    return data;
+
+};
+
+export { fetchMonthData, postDayData };
--- frontend/src/context/TimeSheetContext.jsx
+++ frontend/src/context/TimeSheetContext.jsx
@@ -1,25 +1,11 @@
 import React, { createContext, useContext, useState } from 'react';
+import { fetchMonthData, postDayData } from '../components/api/api';
+import { createKey } from '../utils/utils';
 
 const TimeSheetContext = createContext(undefined);
 
 export function TimeSheetProvider({ children }) {
     const [timeSheetData, setTimeSheetData] = useState({});
-
-    const isValidDate = (date) => {
-        return date instanceof Date && !isNaN(date);
-    }
-
-    const createKey = (date = new Date()) => {
-        if (!isValidDate(date)) {
-            throw new Error('Invalid date input. Please provide a valid Date object or a valid date string.');
-        }
-
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-
-        return `${year}-${month}-${day}`;
-    };
 
     const getDayData = (date) => {
         const key = createKey(date);
@@ -43,38 +29,6 @@ export function TimeSheetProvider({ children }) {
         });
 
         return newMonthData;
-    };
-
-    // Get month data from the backend; Assumes 0 based months
-    const fetchMonthData = async (year, month) => {
-        const response = await fetch(`http://localhost:3000/api/worked-hours/${year}/${month + 1}`);
-        if (!response.ok) {
-            throw new Error('Failed to fetch month data from backend');
-        }
-        const data = await response.json();
-        return data;
-    };
-
-    const postDayData = async (date, formData) => {
-        const url = `http://localhost:3000/api/worked-hours`;
-        const dateId = createKey(date);
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                date: dateId,
-                formData: formData
-            })
-        });
-        if (!response.ok) {
-            throw new Error('Failed post day data to backend');
-        }
-        const data = await response.json();
-        return data;
-
     };
 
     const updateDayData = async (date, formData) => {
